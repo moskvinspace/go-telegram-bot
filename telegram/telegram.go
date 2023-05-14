16a83f6211744b5d138eec5afbf4f187c8f767dd
moskvinspace <me@moskvin.space>
2023-05-14 10:13:04 +0300

    Update telegram.go
    Add BotAPI to struct Bot

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -10,43 +10,40 @@ type Bot struct {
 	Token   string
 	OwnerId string
 	Debug   bool
+	BotAPI  *tgbotapi.BotAPI
 }
 
 func (b *Bot) Start() {
-	bot, err := tgbotapi.NewBotAPI(b.Token)
+	var err error
+
+	b.BotAPI, err = tgbotapi.NewBotAPI(b.Token)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	bot.Debug = b.Debug
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	b.BotAPI.Debug = b.Debug
+	log.Printf("Authorized on account %s", b.BotAPI.Self.UserName)
 
 	config := tgbotapi.UpdateConfig{Timeout: 60}
-	updates := getBotUpdatesChannel(bot, config)
+	updates := b.BotAPI.GetUpdatesChan(config)
 
 	initCommands()
 
 	// Loop through each update.
-	b.checkingBotUpdates(bot, updates)
+	b.processingUpdates(updates)
 }
 
-func (b *Bot) checkingBotUpdates(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel) {
+func (b *Bot) processingUpdates(updates tgbotapi.UpdatesChannel) {
 	for update := range updates {
-		if b.isOwnerExist() {
-			if strconv.FormatInt(update.SentFrom().ID, 10) != b.OwnerId {
-				continue
-			}
+		if b.isOwnerExist() && strconv.FormatInt(update.SentFrom().ID, 10) != b.OwnerId {
+			continue
 		}
 
 		configs := middleware(update)
 		for _, config := range configs {
-			bot.Send(config)
+			b.BotAPI.Send(config)
 		}
 	}
-}
-
-func getBotUpdatesChannel(bot *tgbotapi.BotAPI, config tgbotapi.UpdateConfig) tgbotapi.UpdatesChannel {
-	return bot.GetUpdatesChan(config)
 }
 
 func middleware(update tgbotapi.Update) (configs []tgbotapi.Chattable) {
